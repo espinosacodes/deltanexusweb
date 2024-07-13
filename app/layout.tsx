d416d9ff76fc881d78cfde16e5087678436dd6f0
Santiago :) <santiagoespinosagiraldo1@gmail.com>
2024-07-13 15:34:35 -0500

    feat: Add Hero component and update metadata in layout
    The Hero component is added to the Hero.tsx file, which renders a hero section with multiple Spotlight components. The Spotlight component is also added to the ui folder. This component will be used to showcase important content on the website.
    
    The metadata in the layout.tsx file is updated to reflect the new branding and description of the website.
    
    chore: Update tailwind colors configuration and npm dependencies
    
    The tailwind.config.ts file is updated to include a new color variant for the black color, providing more flexibility in using it in different contexts. Additionally, the package.json file is updated to include new dependencies clsx and tailwind-merge, required for the newly added utility function and for merging Tailwind CSS classes.
    
    chore: Remove unused CSS styles
    
    The globals.css file is updated to remove unused CSS styles related to background colors and text wrapping, improving performance.
    
    chore: Update tailwind colors configuration
    
    The tailwind colors configuration is updated to include a new shade for the black color.
    
    chore: Update npm dependencies
    
    The package.json file is updated to include new dependencies clsx and tailwind-merge.
    
    base file and folder structure
    
    rm
    
    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Delta Nexus | Empowering profitable traders to scale their strategies",
-  description: "Delta Nexus is a algorithmic trading agency dedicated to unlocking the full potential of your trading strategies. ",
+  title:
+    "Delta Nexus | Empowering profitable traders to scale their strategies",
+  description:
+    "Delta Nexus is a algorithmic trading agency dedicated to unlocking the full potential of your trading strategies. ",
 };
 
 export default function RootLayout({
@@ -16,7 +19,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
